35f55a3f36a66d66ad7aa962bd8d075894a3b854
Jay Pandya <91810922+CoderForLife123@users.noreply.github.com>
2023-06-03 17:00:33 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup() {
+    video = createCapture(VIDEO)
+    video.size(550, 500)
+
+    canvas = createCanvas(550, 550)
+    canvas.position(550, 150)
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses)
+}
+
+function draw() {
+    background('#d9534f')
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized!')
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+    }
+}
